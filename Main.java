3e79cd426352b76ebe531e0399380431bce612b5
Ritik-9931 <ritikraushan7372@gmail.com>
2025-12-03 15:04:07 +0530

    Ecommerc

--- Main.java
+++ Main.java
@@ -1,322 +1,288 @@
-package Library_Management;
+package E_Commerce;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.security.PrivateKey;
+import java.util.*;
 
-class BookNotAvailableException extends Exception {
-    public BookNotAvailableException(String msg){
+class OutOfStockException extends Exception{
+    public OutOfStockException(String msg){
         super(msg);
     }
 }
 
-class MaxBookLimitException extends Exception {
-    public MaxBookLimitException(String msg){
+class InvalidProductException extends Exception{
+    public InvalidProductException(String msg){
         super(msg);
     }
 }
 
-class Book{
-    private int id;
-    private String name;
-    private String authorname;
-    private String genre;
-    private boolean isBorrow;
+class Product {
+    private int productId;
+    private String productName;
+    private String category;
+    private int stock;
+    private double price;
 
-    public Book(int id, String name, String authorname, String genre){
-        this.id = id;
-        this.name = name;
-        this.authorname = authorname;
-        this.genre = genre;
-        this.isBorrow = false;
+    public Product(int productId, String productName, String category, int stock, double price){
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.stock = stock;
+        this.price = price;
     }
 
-    public int getId() {
-        return id;
+    public double getPrice(){
+        return this.price;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getProductName() {
+        return productName;
     }
 
-    public String getName() {
-        return name;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void manageStock(int n)throws OutOfStockException{
+        if(stock < n){
+            throw new OutOfStockException("Not Enough Product");
+        }
+        stock -= n;
     }
 
-    public String getAuthorname() {
-        return authorname;
-    }
-
-    public void setAuthorname(String authorname) {
-        this.authorname = authorname;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public boolean isBorrow() {
-        return isBorrow;
-    }
-
-    public void setBorrow(boolean borrow) {
-        isBorrow = borrow;
-    }
-
-    @Override
-    public String toString() {
-        return  "Id='" + id + '\''+
-                ", name='" + name + '\'' +
-                ", Author Name='" + authorname + '\'' +
-                ", Genre='" + genre + '\'' +
-                ", Borrow=" + isBorrow;
+    public String toString(){
+        return "ID: " + productId + " | Name: " + productName + " | Category: " + category + " | Stock: " + stock + " | Price" + price;
     }
 }
 
-class Member{
-    private String name;
-    private int id;
-    private ArrayList<Book> borrow;
+class Cart {
+    ArrayList<Product> cartItems = new ArrayList<>();
 
-    public Member(int id, String name){
-        this.name = name;
-        this.id = id;
-        this.borrow = new ArrayList<>();
+    public void addCart(Product p){
+        cartItems.add(p);
     }
 
-    public String getName() {
-        return name;
+    public ArrayList<Product> getcartItems(){
+        return cartItems;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public ArrayList<Book> getBorrow() {
-        return borrow;
-    }
-
-    public void setBorrow(ArrayList<Book> borrow) {
-        this.borrow = borrow;
-    }
-
-    public void borrowBook(Book book) throws MaxBookLimitException {
-        if(borrow.size() >= 3) {
-            throw new MaxBookLimitException("A member can borrow max 3 books.");
-        }
-        borrow.add(book);
-    }
-
-    public void returnBook(Book book) {
-        borrow.remove(book);
-    }
-
-    @Override
-    public String toString() {
-        return id + " | " + name + " | Borrowed: " + borrow.size();
+    public void clearCart(){
+        cartItems.clear();
     }
 }
 
-class Library{
-    ArrayList<Book> books = new ArrayList<>();
-    ArrayList<Member> members = new ArrayList<>();
-    private String[] genres = {"Fiction", "Science", "History", "Biography", "Mystery"};
+class User {
+    private String userName;
+    private ArrayList<Order> orders = new ArrayList<>();
 
-    public String getGenres() {
-        return genres[0]+" | "+genres[1]+" | " +genres[2]+" | "+genres[3]+" | "+genres[4];
+    public User(String userName, ArrayList<Order> orders){
+        this.userName = userName;
+        this.orders = orders;
     }
 
-    public void setGenres(String[] genres) {
-        this.genres = genres;
+    public String toString(){
+        return "User Name: " + userName + " => " + orders;
     }
+}
 
-    public void addBooks(Book book){
-        books.add(book);
-        System.out.println("Add Book Successfully...");
-    }
+class Order {
+    private static int count = 0;
+    private int productId;
+    private double totalAmount;
+    private Date time;
 
-    public void registerMember(Member member){
-        members.add(member);
-        System.out.println("Member Register Successfully...");
-    }
+    public Order(ArrayList<Product> products){
+        count = ++count;
+        this.productId = count;
 
-    public Book searchBook(String booktile){
-        for(Book book : books){
-            if (book.getName().contains(booktile)){
-                return book;
-            }
+        double sum = 0;
+        for(Product p : products){
+            sum+=p.getPrice();
         }
-        System.out.println("Book Not found...!");
-        return null;
+        this.totalAmount = sum;
+        this.time = new Date();
     }
 
-    public Member findMember(int id){
-        for(Member member : members){
-            if(member.getId() == id){
-                return member;
-            }
-        }
-        System.out.println("Not Found...!");
-        return null;
+    public String toString(){
+        return "\nProduct ID: " + productId + " | Total Amount: " + totalAmount + " | Time: " + time;
     }
-
-    public void borrowBook(int memberId, String bookName) throws BookNotAvailableException, MaxBookLimitException {
-        Member member = findMember(memberId);
-        Book book = searchBook(bookName);
-
-        if(book == null || book.isBorrow()){
-            throw new BookNotAvailableException("The Book is not available");
-        }
-
-        member.borrowBook(book);
-        book.setBorrow(true);
-
-        System.out.println("Book Borrowed Successfully...");
-    }
-
-    public void returnBook(int memberId, String bookName){
-        Member member = findMember(memberId);
-        Book book = searchBook(bookName);
-
-        if(member != null || book != null) {
-            member.returnBook(book);
-            book.setBorrow(false);
-            System.out.println("Book Return Successfully...");
-        }
-    }
-
-    public void viewMemberAndBorrowBooks(){
-        for(Member m : members){
-            System.out.println(m);
-            if(m.getBorrow().isEmpty()){
-                System.out.println(" No Borrowed Books");
-            }else {
-                for(Book b : m.getBorrow()){
-                    System.out.println(" -> " + b);
-                }
-            }
-        }
-    }
-
 }
 
 public class Main {
+    Cart cart = new Cart();
+    ArrayList<Order> orderHistory = new ArrayList<>();
+    ArrayList<Product>products = new ArrayList<>();
+    ArrayList<User> users = new ArrayList<>();
 
-    public static void main() {
-        Library library = new Library();
+    private void addProduct() throws InvalidProductException{
         Scanner sc = new Scanner(System.in);
+        String[] category = {"Books", "Clothing", "Grocery", "Electronics"};
 
-        while (true) {
-            System.out.println("\n--------- Choose one -------");
-            System.out.println("1. Add Book");
-            System.out.println("2. Register Member");
-            System.out.println("3. Borrow Book");
-            System.out.println("4. Return Book");
-            System.out.println("5. Search Book");
-            System.out.println("6. View All Members & Borrowed Books");
-            System.out.println("7. View Book List");
-            System.out.println("8. Exit");
-            System.out.print("Enter your choice : ");
-            int ch = sc.nextInt();
-            switch (ch){
-                case 1:
-                    System.out.print("Enter Book Id: ");
-                    int id = sc.nextInt();
-                    sc.nextLine();
-
-                    System.out.print("Enter Book Name: ");
-                    String name = sc.nextLine();
-
-                    System.out.print("Enter Author Name: ");
-                    String author = sc.nextLine();
-
-                    System.out.println(library.getGenres());
-                    System.out.print("Enter Book Genra: ");
-                    String genra = sc.nextLine();
-
-                    Book book = new Book(id, name, author, genra);
-
-                    library.addBooks(book);
-
-                    break;
-
-                case 2:
-                    System.out.print("Enter member id: ");
-                    int memberId = sc.nextInt();
-
-                    sc.nextLine();
-                    System.out.print("Enter member Name: ");
-                    String memberName = sc.nextLine();
-
-                    Member member = new Member(memberId, memberName);
-
-                    library.registerMember(member);
-                    break;
-
-                case 3:
-                    System.out.print("Enter you id : ");
-                    int memID = sc.nextInt();
-                    sc.nextLine();
-
-                    System.out.println("Enter book title : ");
-                    String title = sc.nextLine();
-                    try {
-                        library.borrowBook(memID, title);
-                    } catch (Exception e){
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-
-                case 4:
-                    System.out.print("Enter you id : ");
-                    int memberID = sc.nextInt();
-                    sc.nextLine();
-
-                    System.out.println("Enter book title : ");
-                    String booktitle = sc.nextLine();
-
-                    library.returnBook(memberID, booktitle);
-                    break;
-
-                case 5:
-                    System.out.println("Enter you book title : ");
-                    sc.nextLine();
-                    String subtitle = sc.nextLine();
-
-                    System.out.println(library.searchBook(subtitle));
-                    break;
-
-                case 6:
-                    library.viewMemberAndBorrowBooks();
-                    break;
-
-                case 7:
-                    for(Book bk : library.books){
-                        System.out.println(bk);
-                    }
-                    break;
-                case 8:
-                    System.out.println("Exit the program.");
-                    return;
-
-                default:
-                    System.out.println("--------- Invalid choice -----------");
+        System.out.print("Enter Product Id: ");
+        int id = sc.nextInt();
+        for(Product p : products){
+            if(p.getProductId() == id){
+                throw new InvalidProductException("This product ID already Available!");
             }
+        }
+        sc.nextLine();
+        System.out.print("Enter Product Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Enter stock: ");
+        int stock = sc.nextInt();
+        int i=1;
+        for (String s : category){
+            System.out.println(" " + i + ". " + s);
+            i++;
+        }
+        System.out.print("Choose options: ");
+        int ch = sc.nextInt();
+        String categories = " ";
+
+        switch (ch){
+            case 1:
+                categories = "Books";
+                break;
+
+            case 2:
+                categories = "Clothing";
+                break;
+
+            case 3:
+                categories = "Grocery";
+                break;
+
+            case 4:
+                categories = "Electronics";
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+                return;
+        }
+        products.add(new Product(id, name, categories, stock, price));
+    }
+
+    private void viewProduct(){
+        Scanner sc = new Scanner(System.in);
+        for(Product p : products){
+            System.out.println(p);
+        }
+    }
+
+    private void searchProduct() throws InvalidProductException{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter product name for search: ");
+        String s = sc.nextLine();
+        String str = s.toLowerCase();
+
+        boolean a = true;
+        for(Product p : products){
+            if((p).getProductName().toLowerCase().contains(str)){
+                System.out.println(p);
+                a = false;
+            }
+        }
+        if(a){
+            throw new InvalidProductException("Product not Found");
+        }
+    }
+
+    private void addCart() throws InvalidProductException, OutOfStockException{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Product ID: ");
+        int pid = sc.nextInt();
+        System.out.print("Enter no. of Product: ");
+        int num = sc.nextInt();
+
+        boolean b = true;
+        for (Product p : products) {
+            if(p.getProductId() == pid) {
+                for(int j=0; j<num; j++) {
+                    cart.addCart(p);
+                }
+                p.manageStock(num);
+                b = false;
+                System.out.println("Add to Cart Successfully");
+            }
+        }
+        if(b){
+            throw new InvalidProductException("Product not Available!");
+        }
+    }
+
+    private void checkOut(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter user Name: ");
+        String user = sc.nextLine();
+
+        orderHistory.add(new Order(cart.getcartItems()));
+        users.add(new User(user, orderHistory));
+        System.out.println("Order Confirmed");
+        cart.clearCart();
+    }
+
+    private void viewHistory(){
+        for (User e : users){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        try {
+            while (true) {
+                System.out.println("""
+                         1. Add Product
+                         2. View Product
+                         3. Search Product
+                         4. Add to cart
+                         5. Checkout
+                         6. View Order History
+                         7. Exit
+                        """);
+                System.out.print("Enter your Choice: ");
+                Scanner sc = new Scanner(System.in);
+                int choice = sc.nextInt();
+
+                switch (choice){
+                    case 1:
+                        m.addProduct();
+                        break;
+
+                    case 2:
+                        m.viewProduct();
+                        break;
+
+                    case 3:
+                        m.searchProduct();
+                        break;
+
+                    case 4:
+                        m.addCart();
+                        break;
+
+                    case 5:
+                        m.checkOut();
+                        break;
+
+                    case 6:
+                        m.viewHistory();
+                        break;
+
+                    case 7:
+                        System.out.println("Exiting...");
+                        return;
+
+                    default:
+                        System.out.println("Invalid Number!");
+                        return;
+                }
+            }
+        }catch (InvalidProductException | OutOfStockException e){
+            System.out.println(e.getMessage());
         }
     }
 }
